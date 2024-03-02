6f1a00151aff4849d544f4ee4f9860b9ae4b8341
unknown <sarvesh13601@gmail.com>
2024-03-02 18:24:48 +0530

    Palindrome added

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 a = 200;
 b = 7;
 console.log(a*b)
+
+
+function isPalindrome(str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+    // Compare characters from start and end of the string
+    for (let i = 0; i < Math.floor(str.length / 2); i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Example usage
+let inputString = "A man, a plan, a canal, Panama!";
+console.log(`"${inputString}" is a palindrome:`, isPalindrome(inputString));
